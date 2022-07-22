9e14ec470c3d0568f86cb9f6b88545b99d7a81d6
unknown <byiringoroscar@gmail.com>
2022-07-22 17:34:42 +0200

    fix instance

--- utilis.py
+++ utilis.py
@@ -3,10 +3,11 @@ from decouple import config
 
 api_token = config('API_TOKEN')
 whatsapp_group_id = config('WHATSAPP_ID')
+instance = config('INSTANCE')
 
 
 def send_whatsapp_message(message):
-    url = "https://api.ultramsg.com/instance12634/messages/chat"
+    url = f"https://api.ultramsg.com/{instance}/messages/chat"
 
     payload = f"token={api_token}&to={whatsapp_group_id}&body={message}&priority=1&referenceId="
     headers = {'content-type': 'application/x-www-form-urlencoded'}
